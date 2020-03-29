d3f57c742bdfe26159358f6a0b770be9102b9404
Caio Cesar Ferreira <caio.c.f@hotmail.com>
2020-03-29 19:28:24 -0300

    Refatorando o banner e titilos em componentes full-banner e list-titles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { FullBannerComponent } from './shared/full-banner/full-banner.component';
+import { ListTitlesComponent } from './shared/list-titles/list-titles.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    FullBannerComponent,
+    ListTitlesComponent
   ],
   imports: [
     BrowserModule,
